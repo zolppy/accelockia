c08d206f7e4b67b34506ab6feab87b33d361dc53
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-07-03 18:43:00 -0300

    feat: 404 customizado

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,16 @@
+import Container from "./components/generic/Container";
+
 export default function Custom404() {
   return (
     <>
-      <p>404 | Página não encontrada</p>
+      <Container>
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col text-center">
+          <span className="text-6xl md:text-7xl lg:text-9xl">404</span>
+          <span className="text-xl md:text-2xl lg:text-4xl whitespace-nowrap">
+            Página não encontrada
+          </span>
+        </div>
+      </Container>
     </>
   );
 }
